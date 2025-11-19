b40a1508433ba33ff82a01fe766f01c5149fc796
Marjuk Ahmed <marjukahmed113@gmail.com>
2025-11-19 11:18:40 +0600

    Latest update

--- static/js/script.js
+++ static/js/script.js
@@ -75,18 +75,3 @@ btn.on('click', function(e) {
 });
 
 
-  var swiper = new Swiper('.mySwiper', {
-            autoplay: {
-                delay: 5000,
-                disableOnInteraction: false,
-            },
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-            },
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            },
-            loop: true,
-        });
